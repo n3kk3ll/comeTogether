badec4cee572461976a69c052bcb51656a57ca15
Andrii <example.gmail.com>
2022-01-12 15:41:16 +0200

    added overlay to mobile menu, fixed bugs in menu toggle

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,20 @@
 const menuToggle = document.getElementById('menuToggle'),
     menu = document.querySelector('.header__nav'),
-    label = document.querySelector('.header__menu-button-sm-label');
+    label = document.querySelector('.header__menu-button-sm-label'),
+    overlay = document.querySelector('.overlay');
 
-menuToggle.addEventListener('change', () => {
+menuToggle.addEventListener('input', () => {
     menu.classList.toggle('open');
+    overlay.classList.toggle('active');
+    document.body.style.overflow = 'hidden';
+    if (menuToggle.checked === false) {
+        document.body.style.overflow = 'visible';
+    }
+});
+
+overlay.addEventListener('click', () => {
+    overlay.classList.remove('active');
+    menu.classList.remove('open');
+    menuToggle.checked = false;
+    document.body.style.overflow = 'visible';
 });
