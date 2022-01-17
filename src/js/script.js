fd0239bef8957b112f05b1ce5978e14b7640c365
Andrii <example.gmail.com>
2022-01-18 01:46:18 +0200

    layout: finished carousel block, remains only to make it responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,18 @@
 const menuToggle = document.getElementById('menuToggle'),
     menu = document.querySelector('.header__nav'),
     label = document.querySelector('.header__menu-button-sm-label'),
-    overlay = document.querySelector('.overlay');
+    overlay = document.querySelector('.overlay'),
+    swiper = new Swiper('.carousel__slider', {
+        navigation: {
+            nextEl: document.querySelector('.carousel__controls-next'),
+            prevEl: document.querySelector('.carousel__controls-prev'),
+        },
+        slidesPerView: 1.15,
+        spaceBetween: 60,
+        loop: true,
+        autoHeight: true,
+        watchOverflow: true,
+    });
 
 menuToggle.addEventListener('input', () => {
     menu.classList.toggle('open');
