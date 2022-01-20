5681835106e2b5268fefb76fc25a2ed076b16859
Zhemchuzhnyi Andrii <n3kk3ll@gmail.com>
2022-01-20 22:27:04 +0200

    finished main page

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ const menuToggle = document.getElementById('menuToggle'),
     menu = document.querySelector('.header__nav'),
     label = document.querySelector('.header__menu-button-sm-label'),
     overlay = document.querySelector('.overlay'),
+    btnUp = document.querySelector('.pageup'),
     swiper = new Swiper('.carousel__slider', {
         navigation: {
             nextEl: document.querySelector('.carousel__controls-next'),
@@ -24,18 +25,41 @@ const menuToggle = document.getElementById('menuToggle'),
         },
     });
 
-menuToggle.addEventListener('input', () => {
+function menuOpen () {
     menu.classList.toggle('open');
     overlay.classList.toggle('active');
     document.body.style.overflow = 'hidden';
     if (menuToggle.checked === false) {
         document.body.style.overflow = 'visible';
     }
-});
+}
 
-overlay.addEventListener('click', () => {
+function menuClose () {
     overlay.classList.remove('active');
     menu.classList.remove('open');
     menuToggle.checked = false;
     document.body.style.overflow = 'visible';
+}
+
+function showScrollButton () {
+    if (window.scrollY >= 1000) {
+        btnUp.style.visibility = 'visible';
+        btnUp.style.opacity = 1;
+        btnUp.style.transition = 'opacity .5s ease';
+    } else {
+        btnUp.style.visibility = 'hidden';
+        btnUp.style.opacity = 0;
+    }
+}
+
+menuToggle.addEventListener('input', () => {
+    menuOpen();
+});
+
+overlay.addEventListener('click', () => {
+    menuClose();
+});
+
+window.addEventListener('scroll', () => {
+    showScrollButton();
 });
